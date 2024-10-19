7d50d3549d3958c503d0265025bc9392db482c07
Levy Buarque <levy.buarque@gmail.com>
2024-10-18 21:19:29 -0300

    feat(404)
    ajustes e troca de nome

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom';
 function Header () {
   return (
     <header>
-      <Link className='logo' to="/">Prime Flix</Link>
+      <Link className='logo' to="/">Filmaria App Flix</Link>
       <Link className='favoritos' to="/favoritos">Meus filmes</Link>
     </header>
   )
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -5,9 +5,6 @@ import './home.css';
 
 // URL DA API:  /movie/now_playing?api_key=323f35eb72dc6ddeda3c36020406b9fc
 
-
-
-
 function Home() {
 
   const [filmes, setFilmes] = useState([]);
