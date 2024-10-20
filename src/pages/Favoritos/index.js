d25b4ca1a7d2b6500c3bc7310f487e9969b6f7db
Levy Buarque <levy.buarque@gmail.com>
2024-10-20 17:21:20 -0300

    feat(meus filmes)
    metodo de excluir e acessar detalhes nos meus filmes

--- src/pages/Favoritos/index.js
+++ src/pages/Favoritos/index.js
@@ -10,17 +10,28 @@ function Favoritos() {
     setFilmes(JSON.parse(minhaLista) || []);
   }, [])
 
+  function exluirFilme(id) {
+    let filtroFilmes = filmes.filter((filme) => {
+      return (filme.id !== id);
+    })
+
+    setFilmes(filtroFilmes);
+    localStorage.setItem("@filmariaapp", JSON.stringify(filtroFilmes));
+  }
+
   return(
     <div className="meus-filmes">
       <h1>Meus Filmes</h1>
+
+      {filmes.length === 0 && <span>Você não possui nenhum filme salvo :/</span>}
       <ul>
         {filmes.map((filme) => {
           return (
             <li key={filme.id}>
               <span>{filme.title}</span>
-              <div>
+              <div className="controls">
                 <Link to={`/filme/${filme.id}`}>Ver detalhes</Link>
-                <button>Excluir</button>
+                <button onClick={() => exluirFilme(filme.id)}>Excluir</button>
               </div>
             </li>
           )
